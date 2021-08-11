4846b6661b55da7ec559787b00752c82eeb795df
Carl Elbaz <carl.elbaz@gmail.com>
2021-08-11 18:04:31 +0200

    initialize project

---
+++ app/src/main/java/com/example/foodist/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.foodist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodist.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
