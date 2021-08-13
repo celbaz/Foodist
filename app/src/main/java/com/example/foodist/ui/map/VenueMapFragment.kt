1f9f72355c0be764ccb18db77ba811edec34fe7b
Carl Elbaz <celbaz9@gmail.com>
2021-08-13 15:55:14 +0200

    Add PermissionFragment, Update VenueMap to show current location

--- app/src/main/java/com/example/foodist/ui/map/VenueMapFragment.kt
+++ app/src/main/java/com/example/foodist/ui/map/VenueMapFragment.kt
@@ -1,22 +1,61 @@
 package com.example.foodist.ui.map
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.foodist.R
+import com.example.foodist.ui.permission.PermissionRequest
+import com.example.foodist.ui.permission.PermissionsFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 /**
- * The **VenueMapFragment** is responsible for displaying a map with points of interest nearby.
+ * The **VenueMapFragment** is responsible for displaying the map.
+ * There are three primary operations that occur here:
+ *  - The fragment will prompt the user for location permissions.
+ *  - The map search area will be set using either the current location or the last viewed area
+ *  - The map will populate with results as the user panes.
  */
-class VenueMapFragment : Fragment() {
+class VenueMapFragment : PermissionsFragment() {
+
+  private var mapLoaded: Boolean = false
+  private lateinit var googleMap: GoogleMap
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    checkGrantedPermissions(PermissionRequest.LOCATION) {}
+  }
 
   override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
+    inflater: LayoutInflater,
+    container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
     return inflater.inflate(R.layout.fragment_venue_map, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?
+    mapFragment?.getMapAsync { gMap ->
+      googleMap = gMap
+      mapLoaded = true
+      setCurrentLocation()
+    }
+  }
+
+  private fun setCurrentLocation() {
+    val amsterdam = LatLng(52.36, 4.90)
+    googleMap.addMarker(MarkerOptions().position(amsterdam).title("Marker in Amsterdam"))
+    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(amsterdam, DEFAULT_ZOOM_LEVEL))
+  }
+
+  companion object {
+    const val DEFAULT_ZOOM_LEVEL = 15.0f
   }
 }
---
+++ app/src/main/java/com/example/foodist/ui/permission/PermissionFragment.kt
@@ -0,0 +1,116 @@
+package com.example.foodist.ui.permission
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.example.foodist.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+/**
+ * The **PermissionsFragment** is a parent class that Fragments can inherit
+ * in order to attempt to ascertain permissions required by a feature.
+ *
+ * To use you must call the method **checkGrantedPermissions** with the following parameters:
+ *    - permission: PermissionRequest
+ *    - onComplete: (success: Boolean) -> Void
+ */
+open class PermissionsFragment : Fragment() {
+  private lateinit var onPermissionRequestComplete: (success: Boolean) -> Unit
+  private val registerForActivity = registerForActivityResult(
+    ActivityResultContracts.RequestMultiplePermissions()
+  ) { result ->
+    val isGranted = result.values.all { granted -> granted == true }
+    onPermissionRequestComplete(isGranted)
+  }
+
+  fun checkGrantedPermissions(permission: PermissionRequest, onComplete: (success: Boolean) -> Unit) {
+    onPermissionRequestComplete = onComplete
+
+    if (arePermissionsGranted(permission)) {
+      onPermissionRequestComplete(true)
+    } else {
+      requestPermissionsReady(permission)
+    }
+  }
+
+
+  private fun arePermissionsGranted(permission: PermissionRequest): Boolean {
+    val areAllPermissionsGranted = permission.getPermissionsArray().all {
+      ActivityCompat.checkSelfPermission(
+        requireContext(), it
+      ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    return areAllPermissionsGranted
+  }
+
+  private fun shouldShowPermissionRationaleDialog(permission: PermissionRequest): Boolean {
+    return permission.getPermissionsArray().any { permissionName ->
+      shouldShowRequestPermissionRationale(permissionName)
+    }
+  }
+
+  private fun showPermissionRationaleDialog(permission: PermissionRequest) {
+    val copy = getCopy(permission)
+    MaterialAlertDialogBuilder(requireActivity(), R.style.AppTheme_AlertDialog)
+      .setTitle(copy.title)
+      .setMessage(copy.message)
+      .setCancelable(false)
+      .setPositiveButton(copy.action) { _, _ ->
+        requestPermission(permission)
+      }
+      .show()
+  }
+
+
+  private fun requestPermissionsReady(permission: PermissionRequest) {
+    if (shouldShowPermissionRationaleDialog(permission)) {
+      showPermissionRationaleDialog(permission)
+    } else {
+      requestPermission(permission)
+    }
+
+  }
+
+  private fun requestPermission(permission: PermissionRequest) {
+    val permissionsArray = permission.getPermissionsArray()
+
+    registerForActivity.launch(permissionsArray)
+
+  }
+
+  private fun getCopy(permission: PermissionRequest): PermissionCopy {
+    return when (permission) {
+      PermissionRequest.LOCATION -> PermissionCopy(
+        R.string.location_permission_title,
+        R.string.location_permission_message,
+      )
+    }
+
+  }
+
+  companion object {
+    const val REQUEST_CODE = 1
+  }
+}
+
+
+data class PermissionCopy(
+  @StringRes val title: Int,
+  @StringRes val message: Int,
+  @StringRes val action: Int = R.string.dialog_continue,
+  @StringRes val cancel: Int = R.string.dialog_cancel,
+)
+
+enum class PermissionRequest {
+  LOCATION;
+
+  fun getPermissionsArray(): Array<String> {
+    return when (this) {
+      LOCATION -> arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+  }
+}
